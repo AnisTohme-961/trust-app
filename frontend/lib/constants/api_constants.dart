6253984d7c7db762ac5ed0e053b85c8d5eebd4b6
Änis Tohme <nistohme1@hotmail.com>
2025-11-12 13:57:41 +0200

    request change to dynamic

--- frontend/lib/constants/api_constants.dart
+++ frontend/lib/constants/api_constants.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/foundation.dart';
 
+// class ApiConstants {
+//   static String get baseUrl =>
+//       kIsWeb ? "http://127.0.0.1:8080" : "http://192.168.17.1:8080";
+// }
 class ApiConstants {
-  static String get baseUrl =>
-      kIsWeb ? "http://127.0.0.1:8080" : "http://192.168.17.1:8080";
+  static String get baseUrl => "https://trust-app.onrender.com";
 }
