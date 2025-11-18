182bac835d59f9eb454f40f10f3fe4aabafa7a8a
Änis Tohme <nistohme1@hotmail.com>
2025-11-18 17:55:09 +0200

    working merge 18/11/2025

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -46,7 +46,7 @@ class MyApp extends StatelessWidget {
           ),
         ),
 
-        home: ResponsiveHomePage(),
+        home: ForgotEidPage(),
         routes: appRoutes(),
       ),
     );
