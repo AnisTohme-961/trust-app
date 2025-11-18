1fc8daea9772c267e1ddc7c22d80011a7fb048a5
Johnny-sadaka <johnnysadaka01@gmail.com>
2025-11-18 16:54:34 +0200

    Placed main.go in the root folder and changed the path of the env in main.go

--- frontend/lib/constants/api_constants.dart
+++ frontend/lib/constants/api_constants.dart
@@ -2,5 +2,5 @@ import 'package:flutter/foundation.dart';
 
 class ApiConstants {
   static String get baseUrl =>
-      kIsWeb ? "http://127.0.0.1:8080" : "http://10.0.3.130:8080";
+      kIsWeb ? "http://127.0.0.1:8080" : "http://192.168.0.132:8080";
 }
