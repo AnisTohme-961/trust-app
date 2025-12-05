5f46b5a1e997ed6e5f81ce36a2cdb1fdde3f4c78
Johnny-sadaka <johnnysadaka01@gmail.com>
2025-12-05 13:05:46 +0200

    signup signin Done

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_project/screens/sign_in_screen.dart';
 import '../providers/language_provider.dart';
 import '../providers/protect_access_provider.dart';
 import './providers/font_size_provider.dart';
