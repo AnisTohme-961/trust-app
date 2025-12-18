bcae66832c85dd3a6799429238ed36d21e5820df
Änis Tohme <nistohme1@hotmail.com>
2025-12-18 18:57:00 +0200

    working merge

--- frontend/lib/constants/api_constants.dart
+++ frontend/lib/constants/api_constants.dart
@@ -5,5 +5,5 @@
 class ApiConstants {
   //   static String get baseUrl => "https://trust-app.onrender.com";
 
-  static String get baseUrl => "http://192.168.0.131:8080";
+  static String get baseUrl => "http://192.168.17.1:8080";
 }
